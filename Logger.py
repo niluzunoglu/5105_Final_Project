81dfa45d5b5d870a2069286580d6bd72c09f7397
niluzunoglu <47059818+niluzunoglu@users.noreply.github.com>
2024-12-29 13:26:04 +0300

    Update Logger.py

--- Logger.py
+++ Logger.py
@@ -1,13 +1,15 @@
-import logging
+import os
 
 class Logger:
     @staticmethod
-    def setup_logging(log_file="training.log"):
+    def setup_logging(log_dir="NLP_FINAL_PROJECT/logs", log_file="training.log"):
+        os.makedirs(log_dir, exist_ok=True)
+        log_path = os.path.join(log_dir, log_file)
         logging.basicConfig(
             level=logging.INFO,
             format="%(asctime)s - %(levelname)s - %(message)s",
             handlers=[
-                logging.FileHandler(log_file),
+                logging.FileHandler(log_path),
                 logging.StreamHandler()
             ]
         )
